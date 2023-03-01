890aa6f7d0db4344575683aa04fec4876a59d1c9
Chintan-elite <thecsquare.elite@gmail.com>
2023-03-01 15:08:52 +0530

    spring mvc

---
+++ J028_SpringMVC/src/main/java/com/model/Book.java
@@ -0,0 +1,45 @@
+package com.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="book")
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	
+	@Size(min = 1,message = "Name should not be blank")
+	String name;
+	
+	double price;
+	int qty;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	
+	
+}
